dc075fa4af022b56c9ecd1219fbc6858a3307fa2
xuzihan-010 <z.xu.23@ucl.ac.uk>
2024-05-11 00:58:20 +0100

    finish the popualtion density map

--- static/js/la_population.js
+++ static/js/la_population.js
@@ -17,27 +17,27 @@
 
     function initLineChart(cityName = 'Select a neighborhood') {
         var option = {
-            title: { text: cityName + ' Population Trend' },
+            title: { text: cityName + ' Population Density per kilometer' },
             tooltip: { trigger: 'axis' },
             xAxis: {
                 type: 'category',
                 data: ['2010', '2015', '2020', '2022'],
-                name: 'Year', // Adding legend Year for X-axis
-                nameLocation: 'middle', // Positioning the X-axis label
-                nameGap: 30  // Gap between the X-axis label and numbers
+                name: 'Year',
+                nameLocation: 'middle',
+                nameGap: 30
             },
             yAxis: {
                 type: 'value',
-                name: 'Population',
+               
                 nameLocation: 'middle',
                 nameGap: 50,
                 nameRotate: 90,
                 axisLabel: {
-                    margin: 0  // Increase margin to avoid cutting off labels
+                    margin: 0
                 }
             },
             series: [{
-                name: 'Population',
+                name: 'Density',
                 type: 'line',
                 data: []
             }]
@@ -45,67 +45,73 @@
         lineChart.setOption(option);
     }
     
-    
     function updateLineChart(cityData, cityName) {
-        var populationValues = ['2010', '2015', '2020', '2022'].map(year => cityData[year] || null);
+        var densityValues = ['2010', '2015', '2020', '2022'].map(year => cityData[year] || null);
         lineChart.setOption({
             title: {
-                text: cityName + ' Population Trend'  // Update title dynamically
+                text: cityName + 'Population Trend'
             },
-            yAxis: {
-                name: 'Population', // Ensure yAxis name updates if needed
-            },
+           
             series: [{
                 name: 'Population',
-                data: populationValues
+                data: densityValues
             }]
         });
     }
-    
 
     function initializeChart() {
         Promise.all([
             loadData("https://raw.githubusercontent.com/XuZiHan-010/Casa003.github.io/main/data/los_angeles.geojson", "../data/los_angeles.geojson"),
-            loadData("https://raw.githubusercontent.com/XuZiHan-010/Casa003.github.io/main/data/la_population_data.json", "../data/la_population_data.json")
+            loadData("https://raw.githubusercontent.com/XuZiHan-010/Casa003.github.io/main/data/la_population_density.json", "../data/la_population_density.json"),
+            loadData("https://raw.githubusercontent.com/XuZiHan-010/Casa003.github.io/main/data/la_population_data.json","../data/la_population_data.json")
         ]).then(function(values) {
             var laJson = values[0];
-            var populationData = values[1];
-            const dataLookup = populationData.reduce((acc, item) => {
+            var densityData = values[1];
+            var populationData = values[2];
+
+            const dataLookup = densityData.reduce((acc, item) => {
+                acc[item.Name.toLowerCase().trim()] = item;
+                return acc;
+            }, {});
+
+
+            const populationLookup = populationData.reduce((acc, item) => {
                 acc[item.Name.toLowerCase().trim()] = item;
                 return acc;
             }, {});
 
             var seriesData = laJson.features.map(feature => {
                 var name = feature.properties.name;
-                var popData = dataLookup[name.toLowerCase().trim()];
-                var value = popData ? popData[document.getElementById('year-selector').value] : -1;
+                var density = dataLookup[name.toLowerCase().trim()];
+                var value = density ? density[document.getElementById('year-selector').value] : -1;
                 return { name: name, value: value };
             });
 
             echarts.registerMap('Los Angeles', laJson);
             var option = {
-                title: { text: 'Los Angeles County Population by Neighborhoods (2010 - 2022)', left: 'right' },
+                title: { text: 'Los Angeles County Population Density by Neighborhood ', 
+                subtext: '(2010-2022) per kilometer',
+                left: 'center' },
                 tooltip: {
                     trigger: 'item',
                     formatter: function(params) {
-                        return params.name + ': ' + (params.value > 0 ? params.value : 'No data');
+                        return params.name + ': ' + (params.value > 0 ? params.value + ' per kilometer' : 'No data');
                     }
                 },
                 visualMap: {
                     left: 'left',
                     min: 100,
-                    max: 500000,
+                    max: 5000, 
                     inRange: {
                         color: ['#00FF00', '#ADFF2F', '#FFFF00', '#FFD700', '#FFA500', '#FF4500', '#FF0000']
                     },
                     text: ['High', 'Low'],
                     calculable: true
                 },
-                
                 toolbox: {
                     show: true,
-                    left: 'left',
-                    top: 'top',
+                    left: 'right',
+                    top: 'bottom',
                     feature: {
                         dataView: { readOnly: false },
                         restore: {},
@@ -113,7 +119,7 @@
                     }
                 },
                 series: [{
-                    name: 'Los Angeles Population by Decade',
+                    name: 'Density by Decade',
                     type: 'map',
                     roam: true,
                     map: 'Los Angeles',
@@ -126,13 +132,14 @@
             myChart.hideLoading();
 
             // Set the first city data to be default value for line chart
-            if (populationData.length > 0) {
-                updateLineChart(populationData[0], populationData[0].Name);  // Initialize with the first city's data
+            if (densityData.length > 0) {
+                updateLineChart(densityData[0], densityData[0].Name);  // Initialize with the first city's data
             }
 
+           // Event listeners for map interaction
             myChart.on('click', function(params) {
-                if (dataLookup[params.name.toLowerCase().trim()]) {
-                    updateLineChart(dataLookup[params.name.toLowerCase().trim()], params.name);
+                if (populationLookup[params.name.toLowerCase().trim()]) {
+                    updateLineChart(populationLookup[params.name.toLowerCase().trim()], params.name);
                 }
             });
 
@@ -140,8 +147,8 @@
                 var selectedYear = this.value;
                 seriesData = laJson.features.map(feature => {
                     var name = feature.properties.name;
-                    var popData = dataLookup[name.toLowerCase().trim()];
-                    return { name: name, value: popData ? popData[selectedYear] : -1 };
+                    var density = dataLookup[name.toLowerCase().trim()];
+                    return { name: name, value: density ? density[selectedYear] : -1 };
                 });
                 myChart.setOption({
                     series: [{ data: seriesData }]
